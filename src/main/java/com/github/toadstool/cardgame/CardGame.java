2db0030af2071318a8d3e49558e43ef51c79badb
Toadstool2675 <leepinut4808@naver.com>
2022-01-15 01:05:28 +0900

    init

---
+++ src/main/java/com/github/toadstool/cardgame/CardGame.java
@@ -0,0 +1,17 @@
+package com.github.toadstool.cardgame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CardGame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
